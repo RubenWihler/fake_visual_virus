062728fa630395f85bbcdc873841aa35814f85d2
Ruben Wihler <wihlerruben@gmail.com>
2022-02-12 00:58:22 +0100

    add example image

--- fake_visual_virus/screenMelter/Source.cpp
+++ fake_visual_virus/screenMelter/Source.cpp
@@ -10,7 +10,7 @@ A fake virus very visual to make a joke.
 #include <shellapi.h>
 
 int scrWidth, scrHeight;
-int Interval = 125;
+int Interval = 75;
 int runtime = 0;
 
 struct beepParams
@@ -52,8 +52,8 @@ LRESULT CALLBACK Main(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 		int X = (rand() % scrWidth) - (150 / 2),
 			Y = (rand() % 15),
 			Width = (rand() % 150);
-		BitBlt(Window, X, Y, Width, scrHeight, Window, X, 0, SRCCOPY);
 		BitBlt(Window, X, Y, Width, scrHeight, Window, X, 0, SRCINVERT);
+		BitBlt(Window, X, Y, Width, scrHeight, Window, X, 0, SRCCOPY);
 		BitBlt(Window, X, Y, Width, scrHeight, Window, X, 0, SRCPAINT);
 		ReleaseDC(hWnd, Window);
 		break;
@@ -75,15 +75,15 @@ int APIENTRY WinMain(HINSTANCE Inst, HINSTANCE Prev, LPSTR Cmd, int showcmd)
 	scrWidth = GetSystemMetrics(SM_CXSCREEN);
 	scrHeight = GetSystemMetrics(SM_CYSCREEN);
 
-	ShellExecute(0, 0, "https://chrome.google.com/webstore/detail/clickclean/ghgabhipcejejjmhhchfonmamedcbeod?hl=en", 0, 0, SW_SHOW);
-	ShellExecute(0, 0, "https://chrome.google.com/webstore/detail/clickclean/ghgabhipcejejjmhhchfonmamedcbeod?hl=en", 0, 0, SW_NORMAL);
-	Sleep(2000);
-	MessageBox(0, "Trust installer blocked the threat!", "A threat was detected.", MB_DEFBUTTON1);
-	Sleep(100);
-	MessageBox(0, "Trust installer blocked the threat!", "A threat was detected.", MB_DEFBUTTON1);
-	Sleep(1000);
-	MessageBox(0, "Trust installer blocked the threat!", "A threat was detected.", MB_DEFBUTTON1);
-	Sleep(2000);
+	//ShellExecute(0, 0, "https://chrome.google.com/webstore/detail/clickclean/ghgabhipcejejjmhhchfonmamedcbeod?hl=en", 0, 0, SW_SHOW);
+	//ShellExecute(0, 0, "https://chrome.google.com/webstore/detail/clickclean/ghgabhipcejejjmhhchfonmamedcbeod?hl=en", 0, 0, SW_NORMAL);
+	//Sleep(2000);
+	//MessageBox(0, "Trust installer blocked the threat!", "A threat was detected.", MB_DEFBUTTON1);
+	//Sleep(100);
+	//MessageBox(0, "Trust installer blocked the threat!", "A threat was detected.", MB_DEFBUTTON1);
+	//Sleep(1000);
+	//MessageBox(0, "Trust installer blocked the threat!", "A threat was detected.", MB_DEFBUTTON1);
+	//Sleep(2000);
 
 	WNDCLASS wndClass = { 0, Main, 0,0, Inst, 0, LoadCursor(0, IDC_ARROW), 0, 0, "screenMelter" };
 
@@ -101,10 +101,10 @@ int APIENTRY WinMain(HINSTANCE Inst, HINSTANCE Prev, LPSTR Cmd, int showcmd)
 			std::thread t(beepTone, params);
 			t.detach();
 
-			for (int i = 0; i < 3; i++)
-			{
-				MessageBox(0, "Trust installer blocked the threat!", "A threat was detected.", MB_ICONEXCLAMATION);	
-			}
+			//for (int i = 0; i < 3; i++)
+			//{
+			//	MessageBox(0, "Trust installer blocked the threat!", "A threat was detected.", MB_ICONEXCLAMATION);	
+			//}
 			while (Msg.message != WM_QUIT)
 			{
 				if (PeekMessage(&Msg, 0, 0, 0, PM_REMOVE))
